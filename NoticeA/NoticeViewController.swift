6dbfae163f0f31159b6961a7dcb0997201ff7a2c
jek01680 <jek01680@icloud.com>
2022-03-18 23:32:54 +0900

    Firebase Analytics , ABtest sample Notice app

---
+++ NoticeA/NoticeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NoticeViewController.swift
+//  NoticeA
+//
+//  Created by 정은경 on 2022/03/18.
+//
+
+import UIKit
+
+class NoticeViewController: UIViewController {
+    var noticeContents: (title: String, detail: String, date: String)?
+    
+    @IBOutlet weak var noticeView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        noticeView.layer.cornerRadius = 6
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        
+        guard let noticeContents = noticeContents else { return }
+        titleLabel.text = noticeContents.title
+        detailLabel.text = noticeContents.detail
+        dateLabel.text = noticeContents.date
+    }
+    
+    @IBAction func tapDoneButton(_ sender: UIButton) {
+    }
+    
+}
